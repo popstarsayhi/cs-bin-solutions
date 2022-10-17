cd9d8e91e4cfba532c07e3f698c2bdf2561dffcc
popstarsayhi <104924605+popstarsayhi@users.noreply.github.com>
2022-10-17 16:46:39 -0400

    Update callback.js

--- callback.js
+++ callback.js
@@ -236,3 +236,46 @@ const floored = function(num) { return Math.floor(num); };
 console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
 
 
+// Challenge 23
+function myFunc(array, callback) {
+  
+  let flag = true
+  for(let i = 0; i< array.length; i++){
+    if(callback(array[i])){
+      return i
+      flag = false
+    }
+  }
+  
+  if(flag) return -1
+}
+
+const numbers = [2, 3, 6, 64, 10, 8, 12];
+const evens = [2, 4, 6, 8, 10, 12, 64];
+
+function isOdd(num) {
+  return (num % 2 !== 0);
+}
+
+// /*** Uncomment these to check your work! ***/
+console.log(myFunc(numbers, isOdd)); // Output should be 1
+console.log(myFunc(evens, isOdd)); // Output should be -1
+
+// Challenge 24
+function myForEach(array, callback) {
+  let sum = 0
+  for(let i = 0; i<array.length; i++){
+    sum += callback(array[i])
+  }
+  return sum
+}
+
+let sum = 0;
+function addToSum(num) {
+  sum += num;
+}
+
+// /*** Uncomment these to check your work! ***/
+const nums = [1, 2, 3];
+myForEach(nums, addToSum);
+console.log(sum); // Should output 6
