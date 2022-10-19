d971edd7e87ba8823e9802d5a82630262f2da750
popstarsayhi <104924605+popstarsayhi@users.noreply.github.com>
2022-10-19 10:18:20 -0400

    Create async.js

---
+++ async.js
@@ -0,0 +1,24 @@
+/* CHALLENGE 1 */
+
+function sayHowdy() {
+  console.log('Howdy');
+}
+
+function testMe() {
+  setTimeout(sayHowdy, 0);
+  console.log('Partnah');
+}
+// After thinking it through, uncomment the following line to check your guess!
+testMe(); // what order should these log out? Partnah first
+
+
+/* CHALLENGE 2 */
+
+function delayedGreet() {
+  setTimeout(function(){
+    console.log("welcome")
+  },3000)
+}
+// Uncomment the following line to check your work!
+delayedGreet(); // should log (after 3 seconds): welcome
+
