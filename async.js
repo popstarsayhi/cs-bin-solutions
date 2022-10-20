252364cbffb877faf1bb3c72ea6dbed9c30fa1f8
popstarsayhi <104924605+popstarsayhi@users.noreply.github.com>
2022-10-19 23:26:07 -0400

    Update async.js

--- async.js
+++ async.js
@@ -34,3 +34,53 @@ function helloGoodbye() {
 // Uncomment the following line to check your work!
 helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
 
+
+/* CHALLENGE 4 */
+
+function brokenRecord() {
+  setInterval(()=>console.log("hi again"),1000)
+}
+// Uncomment the following line to check your work!
+brokenRecord(); // should log (every second): hi again
+
+
+/* CHALLENGE 6 */
+
+function everyXsecsForYsecs(func, interval, duration) {
+  
+  let intervalId = setInterval(func,interval*1000)
+  setTimeout(()=>clearInterval(intervalId),duration*1000)
+}
+// Uncomment the following lines to check your work!
+function theEnd() {
+ console.log('This is the end!');
+}
+everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+/* CHALLENGE 7 */
+
+function delayCounter(target, wait) {
+  let counter = 0
+  let intervalId
+  
+  return function inner(){
+    if(counter === 0){
+       counter++
+       intervalId = setInterval(()=>console.log(inner()),wait)
+    }else if(counter === target){
+      clearInterval(intervalId)
+      return counter
+    }else{
+      return counter++
+    }
+  } 
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const countLogger = delayCounter(3, 1000)
+countLogger();
+//After 1 second, log 1
+//After 2 seconds, log 2
+//After 3 seconds, log 3
+
+
